c8402fb3948ca17c0a0ed3733c698bcc408f66e0
Sheila Miguez <github@codersquid.com>
2015-03-29 18:32:58 -0500

    include helpful admin info for #24

--- conf_site/proposals/admin.py
+++ conf_site/proposals/admin.py
@@ -3,5 +3,21 @@ from django.contrib import admin
 from .models import TalkProposal, TutorialProposal
 
 
-admin.site.register(TalkProposal)
-admin.site.register(TutorialProposal)
+@admin.register(TalkProposal, TutorialProposal)
+class ProposalAdmin(admin.ModelAdmin):
+    list_display = (
+        'number',
+        'title',
+        'speaker_email',
+        'speaker',
+        'kind',
+        'audience_level',
+        'cancelled',
+    )
+    list_filter = (
+        'kind',
+        'audience_level',
+        'cancelled',
+        'recording_release',
+    )
+    date_heirarchy = 'submitted'
