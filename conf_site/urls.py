c655d4f432ba515903985d2cfc1a3f8b07a458fc
David Bertrand <dbertrand@imagescape.com>
2015-04-21 22:22:46 +0000

    Base template theming, removed seattle2015 from urls

--- conf_site/urls.py
+++ conf_site/urls.py
@@ -11,28 +11,23 @@ import symposion.views
 WIKI_SLUG = r"(([\w-]{2,})(/[\w-]{2,})*)"
 
 
-urlpatterns = patterns(
-    "",
-    # url(r"^$", TemplateView.as_view(template_name="pydata-home/index.html"), name="index"),
-    url(r"^$", TemplateView.as_view(template_name="seattle2015/index.html"), name="seattle_index"),
-    url(r"^seattle2015/home/$", TemplateView.as_view(template_name="homepage.html"), name="home"),
-    url(r"^seattle2015/admin/", include(admin.site.urls)),
+urlpatterns = patterns("",
+    url(r"^admin/", include(admin.site.urls)),
 
-    url(r"^seattle2015/account/signup/$", symposion.views.SignupView.as_view(), name="account_signup"),
-    url(r"^seattle2015/account/login/$", symposion.views.LoginView.as_view(), name="account_login"),
-    url(r"^seattle2015/account/", include("account.urls")),
-
-    url(r"^seattle2015/dashboard/", symposion.views.dashboard, name="dashboard"),
-    url(r"^seattle2015/speaker/", include("symposion.speakers.urls")),
-    url(r"^seattle2015/proposals/", include("symposion.proposals.urls")),
-    url(r"^seattle2015/sponsors/", include("symposion.sponsorship.urls")),
-    url(r"^seattle2015/boxes/", include("symposion.boxes.urls")),
-    url(r"^seattle2015/teams/", include("symposion.teams.urls")),
-    url(r"^seattle2015/reviews/", include("symposion.reviews.urls")),
-    url(r"^seattle2015/schedule/", include("symposion.schedule.urls")),
-    url(r"^seattle2015/markitup/", include("markitup.urls")),
-
-    url(r"^seattle2015/", include("symposion.cms.urls")),
+    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r"^account/signup/$", symposion.views.SignupView.as_view(), name="account_signup"),
+    url(r"^account/login/$", symposion.views.LoginView.as_view(), name="account_login"),
+    url(r"^account/", include("account.urls")),
+    url(r"^dashboard/", symposion.views.dashboard, name="dashboard"),
+    url(r"^speaker/", include("symposion.speakers.urls")),
+    url(r"^proposals/", include("symposion.proposals.urls")),
+    url(r"^sponsors/", include("symposion.sponsorship.urls")),
+    url(r"^boxes/", include("symposion.boxes.urls")),
+    url(r"^teams/", include("symposion.teams.urls")),
+    url(r"^reviews/", include("symposion.reviews.urls")),
+    url(r"^schedule/", include("symposion.schedule.urls")),
+    url(r"^markitup/", include("markitup.urls")),
+    url(r"^", include("symposion.cms.urls")),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
