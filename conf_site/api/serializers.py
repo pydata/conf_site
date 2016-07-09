0bc67c2f4d69444be64263f95a15eb60298d8720
Aditya Narayan <narayanaditya95@gmail.com>
2016-07-10 01:01:41 +0530

    Add `absolute_url` field to Speaker API response.

--- conf_site/api/serializers.py
+++ conf_site/api/serializers.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import reverse_lazy
+
 from rest_framework import serializers
 
 from symposion.speakers.models import Speaker, User
@@ -5,7 +7,17 @@ from symposion.speakers.models import Speaker, User
 
 class SpeakerSerializer(serializers.ModelSerializer):
     username = serializers.StringRelatedField(source='user')
+    absolute_url = serializers.SerializerMethodField()
+
+    def get_absolute_url(self, obj):
+        return reverse_lazy('speaker_profile', kwargs={'pk': obj.pk})
 
     class Meta:
         model = Speaker
-        fields = ('username', 'name', 'email')
+        fields = (
+            'username',
+            'name',
+            'email',
+            'absolute_url',
+        )
+
