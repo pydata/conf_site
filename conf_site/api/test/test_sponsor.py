b9f2b5561a96a737c6e8abc525be597917f90f1c
Martey Dodoo <martey@mobolic.com>
2016-12-19 13:33:25 -0500

    Update sponsor test cases for #130.
    Only admin users can see information about sponsors now (see
    d21ead64d40f6d177dfb35a256951a8a05377cb6).

--- conf_site/api/test/test_sponsor.py
+++ conf_site/api/test/test_sponsor.py
@@ -14,7 +14,14 @@ class TestSponsor(TestBase):
         cls.sponsor = Sponsor.objects.first()
 
     def test_sponsor_list_api_anonymous_user(self):
+        """Verify that anonymous users cannot access list of sponsors."""
         response = self.client.get(reverse('sponsor-list'))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_sponsor_list_api_admin_user(self):
+        """Verify that admin users can access list of sponsors."""
+        self.client.login(username="admin@pydata.org", password="admin")
+        response = self.client.get(reverse("sponsor-list"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn(
             {
@@ -30,6 +37,15 @@ class TestSponsor(TestBase):
         )
 
     def test_sponsor_detail_api_anonymous_user(self):
+        """Verify that anonymous users cannot access sponsor details."""
+        response = self.client.get(
+            reverse('sponsor-detail', args=[self.sponsor.pk])
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_sponsor_detail_api_admin_user(self):
+        """Verify that admin users can access sponsor details."""
+        self.client.login(username="admin@pydata.org", password="admin")
         response = self.client.get(
             reverse('sponsor-detail', args=[self.sponsor.pk])
         )
