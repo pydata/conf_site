7903f56f262b0fc97640d61201a5a09ee6113989
Martey Dodoo <martey@mobolic.com>
2017-01-18 21:22:52 -0500

    Allow users to prefer not to answer.

--- conf_site/proposals/models.py
+++ conf_site/proposals/models.py
@@ -10,20 +10,32 @@ class Proposal(ProposalBase):
     AUDIENCE_LEVEL_NOVICE = 1
     AUDIENCE_LEVEL_EXPERIENCED = 2
     AUDIENCE_LEVEL_INTERMEDIATE = 3
+    UNDER_REPRESENTED_YES = "Y"
+    UNDER_REPRESENTED_NO = "N"
+    # https://en.wikipedia.org/wiki/Bartleby,_the_Scrivener
+    UNDER_REPRESENTED_BARTLEBY = "O"
 
     AUDIENCE_LEVELS = [
         (AUDIENCE_LEVEL_NOVICE, "Novice"),
         (AUDIENCE_LEVEL_INTERMEDIATE, "Intermediate"),
         (AUDIENCE_LEVEL_EXPERIENCED, "Experienced"),
     ]
+    UNDER_REPRESENTED_ANSWERS = (
+        ("", "----"),
+        (UNDER_REPRESENTED_YES, "Yes"),
+        (UNDER_REPRESENTED_NO, "No"),
+        (UNDER_REPRESENTED_BARTLEBY, "I would prefer not to answer"),
+    )
 
     audience_level = models.IntegerField(choices=AUDIENCE_LEVELS)
 
-    under_represented_population = models.BooleanField(
+    under_represented_population = models.CharField(
         "Do you feel that you or your talk represent a "
         "population under-represented in the Python "
         "and/or Data community?",
-        default=False)
+        choices=UNDER_REPRESENTED_ANSWERS,
+        default="",
+        max_length=1)
 
     recording_release = models.BooleanField(
         default=True,
