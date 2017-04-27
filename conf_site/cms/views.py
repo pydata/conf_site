c9a9fbdc422c7e74296a1ddc961d270fae3395b6
Martey Dodoo <martey@mobolic.com>
2017-04-27 01:01:15 -0400

    Create custom error page for CSRF errors.
    Create custom template, view, and automated tests for a custom error
    page for users that might encounter CSRF errors when submitting forms on
    conference sites.

--- conf_site/cms/views.py
+++ conf_site/cms/views.py
@@ -1,5 +1,21 @@
+from django.template.response import TemplateResponse
+
 from account.forms import LoginEmailForm
 from account.views import LoginView
+
+
+def csrf_failure(request, reason=""):
+    """
+    Custom view for users who encounter CSRF errors.
+
+    https://docs.djangoproject.com/en/1.9/ref/settings/#csrf-failure-view
+
+    When we upgrade to Django 1.10, this view can be removed.
+
+    """
+    response = TemplateResponse(
+        request=request, template="403_csrf.html", status=403)
+    return response
 
 
 class LoginEmailView(LoginView):
