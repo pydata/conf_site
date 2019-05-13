a16a1b065ad1e91e01f20b19ce15107c1154cbc3
Martey Dodoo <martey@mobolic.com>
2019-05-13 14:04:36 -0400

    Add proposal feedback to Django admin.

--- conf_site/reviews/admin.py
+++ conf_site/reviews/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 
-from conf_site.reviews.models import ProposalVote
+from conf_site.reviews.models import ProposalFeedback, ProposalVote
+
+
+@admin.register(ProposalFeedback)
+class ProposalFeedbackAdmin(admin.ModelAdmin):
+    list_display = ("proposal", "author", "comment", "date_created")
 
 
 @admin.register(ProposalVote)
