4f1714b96c06924f29c0317f3c9dd780860fab60
Martey Dodoo <martey@mobolic.com>
2019-08-21 23:45:19 -0400

    Add view to export proposal submitters.

---
+++ conf_site/proposals/tests/test_exporting_proposal_submitters.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from random import randint
+
+from django.urls import reverse
+
+from conf_site.accounts.tests import AccountsTestCase
+from conf_site.proposals.tests.factories import ProposalFactory
+from conf_site.proposals.views import ExportProposalSubmittersView
+from conf_site.speakers.tests.factories import SpeakerFactory
+
+
+class ExportProposalSubmittersViewTestCase(AccountsTestCase):
+    def test_status_code(self):
+        response = ExportProposalSubmittersView().get()
+        self.assertEqual(response.status_code, 200)
+
+    def test_header_row(self):
+        """Verify that header row appears in CSV file response."""
+        view = ExportProposalSubmittersView()
+        response = view.get()
+        self.assertContains(response, ",".join(view.header_row))
+
+    def test_all_proposals_are_included(self):
+        proposals = ProposalFactory.create_batch(size=randint(2, 4))
+        response = ExportProposalSubmittersView().get()
+        for proposal in proposals:
+            self.assertContains(response, proposal.speaker.name)
+            self.assertContains(response, proposal.speaker.email)
+            self.assertContains(response, proposal.title)
+
+    def test_additional_speakers_are_included(self):
+        """Verify that we are picking up both types of speakers."""
+        proposal = ProposalFactory()
+        additional_speakers = SpeakerFactory.create_batch(size=randint(2, 4))
+        proposal.additional_speakers.add(*additional_speakers)
+        response = ExportProposalSubmittersView().get()
+        for speaker in additional_speakers:
+            self.assertContains(response, speaker.name)
+            self.assertContains(response, speaker.email)
+
+    def test_no_anonymous_access(self):
+        self.client.logout()
+        response = self.client.get(reverse("proposal_submitter_export"))
+        self.assertEqual(response.status_code, 302)
+
+    def test_staff_access(self):
+        self._become_staff()
+        self.client.login(username=self.user.email, password=self.password)
+        response = self.client.get(reverse("proposal_submitter_export"))
+        self.assertEqual(response.status_code, 200)
---
+++ conf_site/proposals/views.py
@@ -0,0 +1,36 @@
+from conf_site.core.views import CsvView
+from conf_site.proposals.models import Proposal
+
+
+class ExportProposalSubmittersView(CsvView):
+    """Export information about proposal submitters."""
+
+    csv_filename = "proposal-submitters.csv"
+    header_row = [
+        "Submitter Name",
+        "Submitter Email",
+        "Proposal Name",
+        "Proposal Type",
+    ]
+
+    def _write_submitter_row(self, submitter, proposal):
+        """Utility method to write a row for an individual submitter."""
+        self.csv_writer.writerow(
+            [
+                submitter.name,
+                submitter.email,
+                proposal.title,
+                proposal.kind.name,
+            ]
+        )
+
+    def get(self, *args, **kwargs):
+        self.csv_writer.writerow(self.header_row)
+
+        # Iterate through proposals.
+        for proposal in Proposal.objects.order_by("title"):
+            self._write_submitter_row(proposal.speaker, proposal)
+            for additional_submitter in proposal.additional_speakers.all():
+                self._write_submitter_row(additional_submitter, proposal)
+
+        return super(ExportProposalSubmittersView, self).get(*args, **kwargs)
