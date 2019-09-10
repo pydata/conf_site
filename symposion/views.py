db0550547922660e9b461f73fc2446a692e51747
Martey Dodoo <martey@mobolic.com>
2019-09-10 13:10:38 -0400

    Add section CSVs to dashboard view.
    Allow "Export" section of dashboard view to include presentation CSVs by
    section by passing all schedules to the context.

--- symposion/views.py
+++ symposion/views.py
@@ -3,10 +3,13 @@ from __future__ import unicode_literals
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from symposion.schedule.models import Schedule
+
 
 @login_required
 def dashboard(request):
     if request.session.get("pending-token"):
         return redirect("speaker_create_token",
                         request.session["pending-token"])
-    return render(request, "dashboard.html")
+    context = {"schedules": Schedule.objects.all()}
+    return render(request, "dashboard.html", context)
