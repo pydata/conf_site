8a4dc5253d5a0ec05848164b904781d4437c96a6
Martey Dodoo <martey@mobolic.com>
2019-10-07 03:28:02 -0400

    Fuzz proposal speaker manage test variables.
    Use faker to make names and email addresses in
    ProposalSpeakerManageViewTestCase's automated tests random.

--- conf_site/proposals/tests/test_proposal_management.py
+++ conf_site/proposals/tests/test_proposal_management.py
@@ -2,9 +2,10 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.utils.crypto import get_random_string
 
-from symposion.speakers.models import Speaker
+from faker import Faker
 
 from conf_site.proposals.tests import ProposalTestCase
+from symposion.speakers.models import Speaker
 
 
 class ProposalSpeakerManageViewTestCase(ProposalTestCase):
@@ -12,17 +13,22 @@ class ProposalSpeakerManageViewTestCase(ProposalTestCase):
 
     def setUp(self):
         super(ProposalSpeakerManageViewTestCase, self).setUp()
+
+        self.faker = Faker()
+
         user_model = get_user_model()
-        USER_EMAIL = "example@example.com"
+        USER_EMAIL = self.faker.email()
         USER_PASSWORD = get_random_string()
         self.user = user_model.objects.create_user(
-            username="user", email=USER_EMAIL, password=USER_PASSWORD
+            username=self.faker.profile()["username"],
+            email=USER_EMAIL,
+            password=USER_PASSWORD,
         )
-        speaker = Speaker.objects.create(name="Nancy Pelosi")
+        speaker = Speaker.objects.create(name=self.faker.name())
         speaker.user = self.user
         speaker.save()
 
-        # Overwrite speaker for this case's proposal - sorry, Paul Ryan.
+        # Overwrite speaker for this case's proposal.
         self.proposal.speaker = speaker
         self.proposal.save()
 
@@ -34,7 +40,7 @@ class ProposalSpeakerManageViewTestCase(ProposalTestCase):
         """Verify that you can't manage a proposal that is not yours."""
         # Create a new speaker and change ownership of this
         # test case's Proposal to said speaker.
-        other_speaker = Speaker.objects.create(name="Other Speaker")
+        other_speaker = Speaker.objects.create(name=self.faker.name())
         self.proposal.speaker = other_speaker
         self.proposal.save()
 
@@ -45,9 +51,10 @@ class ProposalSpeakerManageViewTestCase(ProposalTestCase):
 
     def test_inviting_speaker(self):
         """Verify that inviting a speaker works."""
+        invite_email = self.faker.email()
         response = self.client.post(
             path=reverse("proposal_speaker_manage", args=[self.proposal.pk]),
-            data={"email": "valid@example.com"},
+            data={"email": invite_email},
             follow=True,
         )
         self.assertRedirects(
