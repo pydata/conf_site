f183659b4b99f4193de4ca0ecc6ffb09e4d5b76c
Martey Dodoo <martey@mobolic.com>
2019-10-24 03:58:04 -0400

    Cache scores for individual ProposalVotes.
    Cache individual proposal votes for each user so that proposal list view
    will render faster.

--- conf_site/reviews/models.py
+++ conf_site/reviews/models.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.cache import cache
 from django.core.mail import send_mass_mail
 from django.db import models
 from django.db.models.signals import post_save
@@ -70,10 +72,26 @@ class ProposalVote(models.Model):
         self.comment_html = parse(self.comment)
         return super(ProposalVote, self).save(*args, **kwargs)
 
+    def get_numeric_score_display(self):
+        """Returns numeric value at beginning of score display string."""
+        return self.get_score_display()[0:2].strip()
+
+
+def proposalvote_score_cache_key(proposal, voter):
+    """
+    Return the cache key for a ProposalVote's score
+    based on the proposal and voting user.
+    """
+    return "proposalvote_{}_{}_score".format(proposal.pk, voter.pk)
+
 
 def refresh_vote_counts(sender, instance, created, **kwargs):
     # Update this ProposalVote's proposal's cached proposal votes.
     instance.proposal._refresh_vote_counts()
+    # Update this voter's cached score for this proposal.
+    cache_key = proposalvote_score_cache_key(instance.proposal, instance.voter)
+    numeric_score = instance.get_numeric_score_display()
+    cache.set(cache_key, numeric_score, settings.CACHE_TIMEOUT_LONG)
 
 
 post_save.connect(refresh_vote_counts, sender=ProposalVote)
