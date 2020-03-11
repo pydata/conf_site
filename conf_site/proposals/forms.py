319442bd075a6aac8011e81f8d408fdecc177607
Martey Dodoo <martey@mobolic.com>
2020-03-10 23:34:25 -0400

    Use django-crispy-forms to create form fieldsets.
    Create sections within the base ProposalForm by using
    django-crispy-forms' layout feature.

--- conf_site/proposals/forms.py
+++ conf_site/proposals/forms.py
@@ -1,6 +1,31 @@
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Fieldset, Layout
+
 from conf_site.proposals.models import Proposal
+
+
+SECTION1_LEGEND = (
+    "<h4>The following information will be listed publicly "
+    "in the conference program for a {{ kind.name|lower }}. "
+    "This information should include what the {{ kind.name|lower }} "
+    "is about and why it is interesting, the target audience for "
+    "the {{ kind.name|lower }}, and what attendees will learn.</h4>"
+)
+
+
+SECTION2_LEGEND = (
+    "<h4>The following fields are for the review process "
+    "but are optional and will not be published publicly.</h4>"
+)
+
+
+SECTION3_LEGEND = (
+    "<h4>Additionally, we will ask you some optional questions "
+    "that will not be part of the review process and "
+    "will not be published publicly.</h4>"
+)
 
 
 class ModelMultipleTagChoiceField(forms.ModelMultipleChoiceField):
@@ -47,6 +72,33 @@ class ProposalForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ProposalForm, self).__init__(*args, **kwargs)
 
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Fieldset(
+                SECTION1_LEGEND,
+                "title",
+                "audience_level",
+                "description",
+                "abstract",
+                "affiliation",
+            ),
+            Fieldset(
+                SECTION2_LEGEND,
+                "additional_notes",
+                "first_time_at_jupytercon",
+            ),
+            Fieldset(
+                SECTION3_LEGEND,
+                "requests",
+                "gender",
+                "referral",
+                "under_represented_group",
+                "accomodation_needs",
+            ),
+            "recording_release",
+            "phone_number",
+        )
 
     def clean_description(self):
         value = self.cleaned_data["description"]
