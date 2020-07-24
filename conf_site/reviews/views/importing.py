5293ce9fbf6ed8ca3bb347293b5981ccaf41576f
Martey Dodoo <martey@mobolic.com>
2020-07-24 13:17:37 -0400

    Add reviewer-proposal matching.

--- conf_site/reviews/views/importing.py
+++ conf_site/reviews/views/importing.py
@@ -3,7 +3,10 @@ from django.template.defaultfilters import pluralize
 from django.urls import reverse_lazy
 
 from conf_site.core.views import CsvImportView, SuperuserOnlyView
-from conf_site.reviews.tasks import import_reviewer_csv
+from conf_site.reviews.tasks import (
+    import_reviewer_csv,
+    import_reviewer_proposal_matching_csv,
+)
 
 
 class ReviewerCsvImportView(SuperuserOnlyView, CsvImportView):
@@ -25,4 +28,21 @@ class ReviewerCsvImportView(SuperuserOnlyView, CsvImportView):
         else:
             messages.error(
                 self.request, "Unable to successfully import CSV file."
+            )
+
+
+class ReviewerProposalMatchingCsvImportView(SuperuserOnlyView, CsvImportView):
+    success_url = reverse_lazy("reviewer_matching_import")
+    template_name = "reviews/reviewer_matching_import.html"
+
+    def process(self, filename, user_id):
+        result = import_reviewer_proposal_matching_csv(filename)
+        if result:
+            for error in result["errors"]:
+                messages.error(self.request, error)
+            for warning in result["warnings"]:
+                messages.warning(self.request, warning)
+            messages.success(
+                self.request,
+                "{} reviews requested".format(result["num_reviews_requested"]),
             )
