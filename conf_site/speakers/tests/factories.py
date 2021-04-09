5449420b0eb7a886ee16870bf8ca8a0068cdd846
Martey Dodoo <martey@mobolic.com>
2021-04-09 16:24:59 -0400

    Add speaker time zones to CSV exports.
    Add speaker time zones to proposal and "speakers and presentations"
    exports.

--- conf_site/speakers/tests/factories.py
+++ conf_site/speakers/tests/factories.py
@@ -9,6 +9,7 @@ from conf_site.accounts.tests.factories import UserFactory
 class SpeakerFactory(factory.django.DjangoModelFactory):
     user = factory.SubFactory(UserFactory)
     name = factory.Faker("name")
+    speaker_timezone = factory.Faker("timezone")
 
     class Meta:
         model = Speaker
